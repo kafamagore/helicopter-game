69b5582169da9836fb997fd7f0f5545bc5be0dd2
us <rahmetsaritekin@gmail.com>
2017-11-28 17:52:04 +0300

    fixed time and locate

--- game.py
+++ game.py
@@ -679,21 +679,19 @@ def game_loop():
             if score == 0:
                 start_time = time.time()
                 if int(time.time() - start_time) <= 3:
-                    game_display.blit(message_to_screen("LEVEL: {0}".format(1), font, 50, black), (10, 10))
+                    game_display.blit(message_to_screen("LEVEL: {0}".format(1), font, 50, black), (280, 70))
 
-        if score >= 1000 and score < 2000:
-            start_time = time.time()
-            if int(time.time() - start_time) <= 3:
-                game_display.blit(message_to_screen("SCORE: {0}".format(2), font, 50, black), (10, 10))
+        if score == 1000:
+            game_display.blit(message_to_screen("LEVEL: {0}".format(2), font, 50, black), (280, 70))
 
-        if score > 2000 and score < 3000:
-            game_display.blit(message_to_screen("SCORE: {0}".format(3), font, 50, black), (10, 10))
+        if score == 2000 :
+            game_display.blit(message_to_screen("LEVEL: {0}".format(3), font, 50, black), (280, 70))
 
-        if score > 3000 and score < 4000:
-            game_display.blit(message_to_screen("SCORE: {0}".format(4), font, 50, black), (10, 10))
+        if score == 3000 :
+            game_display.blit(message_to_screen("LEVEL: {0}".format(4), font, 50, black), (280, 70))
 
-        if score > 4000:
-            game_display.blit(message_to_screen("SCORE: {0}".format(5), font, 50, black), (10, 10))
+        if score == 4000:
+            game_display.blit(message_to_screen("LEVEL: {0}".format(5), font, 50, black), (280, 70))
 
         # draw high score
         if score < highscore_int:
