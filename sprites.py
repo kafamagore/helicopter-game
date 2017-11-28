8ec703481d87fdd2c7b9110b8d5b2d54952ae8e9
H. Emre ARI <h.emreari@protonmail.com>
2017-11-28 22:10:09 +0300

    changed pictures

--- sprites.py
+++ sprites.py
@@ -1,28 +1,28 @@
 import pygame
 
-helicopter_1 = pygame.image.load('sprites/helicopter_1.gif')
-helicopter_2 = pygame.image.load('sprites/helicopter_2.gif')
+helicopter_1 = pygame.image.load('sprites/helicopter_1.png')
+helicopter_2 = pygame.image.load('sprites/helicopter_1.png')
 helicopter_crash_1 = pygame.image.load('sprites/helicopter_crash_1.png')
 helicopter_crash_2 = pygame.image.load('sprites/helicopter_crash_2.png')
 helicopter_crash_3 = pygame.image.load('sprites/helicopter_crash_3.png')
 helicopter_crash_4 = pygame.image.load('sprites/helicopter_crash_4.png')
-helicopter_damaged_1 = pygame.image.load('sprites/helicopter_damaged_1.png')
-helicopter_damaged_2 = pygame.image.load('sprites/helicopter_damaged_2.png')
+helicopter_damaged_1 = pygame.image.load('sprites/helicopter_damaged1.png')
+helicopter_damaged_2 = pygame.image.load('sprites/helicopter_damaged1.png')
 
-enemy_helicopter_1 = pygame.image.load('sprites/enemy_helicopter_1.png')
-enemy_helicopter_2 = pygame.image.load('sprites/enemy_helicopter_2.png')
+enemy_helicopter_1 = pygame.image.load('sprites/enemy_helicopter11.png')
+enemy_helicopter_2 = pygame.image.load('sprites/enemy_helicopter11.png')
 
-boat = pygame.image.load('sprites/boat.png')
+boat = pygame.image.load('sprites/boat11.png')
 
 helicopter_list = [helicopter_1, helicopter_2]
 damaged_helicopter_list = [helicopter_damaged_1, helicopter_damaged_2]
 enemy_helicopter_list = [enemy_helicopter_1, enemy_helicopter_2]
 
-balloon = pygame.image.load('sprites/balloon.png')
+balloon = pygame.image.load('sprites/balloon1.png')
 
-spaceship = pygame.image.load('sprites/spaceship.png')
+spaceship = pygame.image.load('sprites/spaceship1.png')
 
-icon = pygame.image.load('sprites/icon.png')
+icon = pygame.image.load('sprites/icon1.png')
 background = pygame.image.load('sprites/background.png')
 cloud = pygame.image.load('sprites/cloud.png')
 
