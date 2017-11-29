8c31da343aa0499bcbaf8c0a50c86277f9d1c996
H. Emre ARI <h.emreari@protonmail.com>
2017-11-29 14:31:47 +0300

    add motion animation to helicopters

--- sprites.py
+++ sprites.py
@@ -1,15 +1,15 @@
 import pygame
 
-helicopter_1 = pygame.image.load('sprites/helicopter_1.png')
+helicopter_1 = pygame.image.load('sprites/helicopter_11.png')
 helicopter_2 = pygame.image.load('sprites/helicopter_1.png')
 helicopter_crash_1 = pygame.image.load('sprites/helicopter_crash_1.png')
 helicopter_crash_2 = pygame.image.load('sprites/helicopter_crash_2.png')
 helicopter_crash_3 = pygame.image.load('sprites/helicopter_crash_3.png')
 helicopter_crash_4 = pygame.image.load('sprites/helicopter_crash_4.png')
 helicopter_damaged_1 = pygame.image.load('sprites/helicopter_damaged1.png')
-helicopter_damaged_2 = pygame.image.load('sprites/helicopter_damaged1.png')
+helicopter_damaged_2 = pygame.image.load('sprites/helicopter_damaged11.png')
 
-enemy_helicopter_1 = pygame.image.load('sprites/enemy_helicopter11.png')
+enemy_helicopter_1 = pygame.image.load('sprites/enemy_helicopter1.png')
 enemy_helicopter_2 = pygame.image.load('sprites/enemy_helicopter11.png')
 
 boat = pygame.image.load('sprites/boat11.png')
